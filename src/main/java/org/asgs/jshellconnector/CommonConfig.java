167973cef5a0310425bd2cd0afe3cf0002ed7055
asgs <asgosa.eeegct@gmail.com>
2017-09-29 18:29:03 +0530

    IntelliJ-triggered code reformatting.

--- src/main/java/org/asgs/jshellconnector/CommonConfig.java
+++ src/main/java/org/asgs/jshellconnector/CommonConfig.java
@@ -3,24 +3,23 @@ package org.asgs.jshellconnector;
 import java.io.IOException;
 import java.util.Properties;
 
-/**
- * Created by asgs on 06-01-2017.
- */
+/** Created by asgs on 06-01-2017. */
 public class CommonConfig {
 
-    private static Properties config = new Properties();
+  private static Properties config = new Properties();
 
-    static {
-        try {
-            config.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("config.properties"));
-        } catch (IOException e) {
-            // Assuming it's a JDK.
-            config.put("JDK_HOME", System.getProperty("java.home"));
-            config.put("WS_PORT", 8080);
-        }
+  static {
+    try {
+      config.load(
+          Thread.currentThread().getContextClassLoader().getResourceAsStream("config.properties"));
+    } catch (IOException e) {
+      // Assuming it's a JDK.
+      config.put("JDK_HOME", System.getProperty("java.home"));
+      config.put("WS_PORT", 8080);
     }
+  }
 
-    public static String getValue(String key) {
-        return config.getProperty(key);
-    }
+  public static String getValue(String key) {
+    return config.getProperty(key);
+  }
 }
